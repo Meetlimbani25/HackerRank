fa0be38565bcc8faf3eb87b99214e02e90732132
Meetlimbani25 <meetlimbani25@gmail.com>
2025-08-19 09:53:52 +0530

    Create Beautiful_Days_Movies.cpp

---
+++ Beautiful_Days_Movies.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to reverse a number
+int reverseNumber(int n) {
+    int rev = 0;
+    while (n > 0) {
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+    return rev;
+}
+
+int beautifulDays(int i, int j, int k) {
+    int count = 0;
+    for (int day = i; day <= j; day++) {
+        int rev = reverseNumber(day);
+        if (abs(day - rev) % k == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int i, j, k;
+    cin >> i >> j >> k;
+    cout << beautifulDays(i, j, k) << endl;
+    return 0;
+}
