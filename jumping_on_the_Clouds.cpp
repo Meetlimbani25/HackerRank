70dddc2b382673378650dbf8b7b9a64d68e11b96
Meetlimbani25 <meetlimbani25@gmail.com>
2025-08-28 07:44:29 +0530

    Create jumping_on_the_Clouds.cpp

---
+++ jumping_on_the_Clouds.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int jumpingOnClouds(vector<int> c) {
+    int n = c.size();
+    int jumps = 0;
+    int i = 0;
+
+    while (i < n - 1) {
+        if (i + 2 < n && c[i + 2] == 0) {
+            i += 2; // jump 2 if safe
+        } else {
+            i += 1; // otherwise jump 1
+        }
+        jumps++;
+    }
+
+    return jumps;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for (int i = 0; i < n; i++) cin >> c[i];
+
+    cout << jumpingOnClouds(c) << endl;
+    return 0;
+}
