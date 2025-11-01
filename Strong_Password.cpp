6733b25fc26e783025b01e767bce2840668c5624
Meetlimbani25 <meetlimbani25@gmail.com>
2025-11-01 12:32:33 +0530

    Create Strong_Password.cpp

---
+++ Strong_Password.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
+int minimumNumber(int n, string password) {
+    bool hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+    string special = "!@#$%^&*()-+";
+
+    for (char c : password) {
+        if (islower(c)) hasLower = true;
+        else if (isupper(c)) hasUpper = true;
+        else if (isdigit(c)) hasDigit = true;
+        else if (special.find(c) != string::npos) hasSpecial = true;
+    }
+
+    int missingTypes = 0;
+    if (!hasLower) missingTypes++;
+    if (!hasUpper) missingTypes++;
+    if (!hasDigit) missingTypes++;
+    if (!hasSpecial) missingTypes++;
+
+    return max(missingTypes, 6 - n);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string password;
+    getline(cin, password);
+
+    int answer = minimumNumber(n, password);
+
+    fout << answer << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+    return s;
+}
